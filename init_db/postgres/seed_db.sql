6d2102e0720172057b85a9dbeb328547df7f9499
robertofedericosoft <59876233+robertofedericosoft@users.noreply.github.com>
2022-02-24 12:50:04 +0100

    Update seed_db.sql
    add elment in seed

--- init_db/postgres/seed_db.sql
+++ init_db/postgres/seed_db.sql
@@ -4,3 +4,4 @@ DROP TABLE IF EXISTS quickdemo CASCADE;
 CREATE TABLE quickdemo(id INT, name VARCHAR(20), value INT);
 INSERT INTO quickdemo VALUES(1,'This',12);
 INSERT INTO quickdemo VALUES(2,'That',345);
+INSERT INTO quickdemo VALUES(3,'New',345);
